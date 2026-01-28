16df994e9b5ba42b59b22fa0069fe55326a4727e
justin <justin@randoneering.tech>
2026-01-27 19:49:03 -0700

    updating starship one last time

--- home/shell/starship.nix
+++ home/shell/starship.nix
@@ -17,34 +17,27 @@
 
         # Popping and Locking color palette format
         format = lib.concatStrings [
-          "[](fg:#1d2021)"
           "  "
           "$username"
-          "[](bg:#3c3836 fg:#1d2021)"
           "$directory"
-          "[](fg:#3c3836 bg:#504945)"
           "$git_branch"
           "$git_status"
-          "[](fg:#504945 bg:#665c54)"
           "$rust"
           "$python"
-          "[](fg:#665c54 bg:#7c6f64)"
           "$docker_context"
-          "[](fg:#7c6f64 bg:#928374)"
           "$time"
-          "[ ](fg:#928374)"
           "\n"
           "$character"
         ];
         username = {
           show_always = true;
-          style_user = "bold bg:#1d2021 fg:#fabd2f";
-          style_root = "bold bg:#1d2021 fg:#f42c3e";
+          style_user = "bold fg:#fabd2f";
+          style_root = "bold fg:#f42c3e";
           format = "[$user ]($style)";
           disabled = false;
         };
         directory = {
-          style = "bold bg:#3c3836 fg:#7ec16e";
+          style = "bold fg:#458588";
           format = "[ $path ]($style)";
           truncation_length = 3;
           truncation_symbol = "…/";
@@ -57,11 +50,11 @@
         };
         git_branch = {
           symbol = "";
-          style = "bold bg:#504945 fg:#b8bb26";
+          style = "bold fg:#b16286";
           format = "[ $symbol $branch ]($style)";
         };
         git_status = {
-          style = "bold bg:#504945 fg:#fabd2f";
+          style = "bold fg:#689d6a";
           format = "[$all_status$ahead_behind ]($style)";
           ahead = "⇡\${count}";
           diverged = "⇕⇡\${ahead_count}⇣\${behind_count}";
@@ -76,23 +69,23 @@
         };
         python = {
           symbol = "";
-          style = "bold bg:#665c54 fg:#fabd2f";
+          style = "bold fg:#fabd2f";
           format = "[ $symbol ($version) ]($style)";
         };
         rust = {
           symbol = "";
-          style = "bold bg:#665c54 fg:#f42c3e";
+          style = "bold fg:#f42c3e";
           format = "[ $symbol ($version) ]($style)";
         };
         docker_context = {
           symbol = "";
-          style = "bold bg:#7c6f64 fg:#99c6ca";
+          style = "bold fg:#99c6ca";
           format = "[ $symbol $context ]($style)";
         };
         time = {
           disabled = false;
           time_format = "%T";
-          style = "bold bg:#928374 fg:#ebdbb2";
+          style = "bold fg:#ebdbb2";
           format = "[  $time ]($style)";
         };
         line_break = {
@@ -100,7 +93,7 @@
         };
         character = {
           disabled = false;
-          success_symbol = "[](bold fg:#458588)";
+          success_symbol = "[](bold fg:#b8bb26)";
           error_symbol = "[󰈸](bold fg:#f42c3e)";
         };
         nix_shell = {
@@ -113,21 +106,21 @@
         };
         aws = {
           format = " [$symbol $profile $region]($style)";
-          style = "bold bg:#7c6f64 fg:#99c6ca";
+          style = "bold fg:#fabd2f";
           symbol = " ";
         };
         gcloud = {
           format = " [$symbol $profile $region]($style)";
-          style = "bold bg:#7c6f64 fg:#99c6ca";
+          style = "bold fg:#458588";
           symbol = "☁️ ";
         };
         azure = {
           format = " [$symbol $profile $region]($style)";
-          style = "bold bg:#7c6f64 fg:#99c6ca";
+          style = "bold fg:#458588";
           symbol = "󰠅 ";
         };
         terraform = {
-          style = "bold #7ec16e";
+          style = "bold #fabd2f";
           format = "[$symbol]($style)";
           symbol = "⬢";
         };
