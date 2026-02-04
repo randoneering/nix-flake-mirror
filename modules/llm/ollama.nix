6a20cf43dd9ef401cd71e0909aee96471622737f
justin <justin@randoneering.tech>
2026-02-04 13:59:16 -0700

    add ollama to unstable

--- modules/llm/ollama.nix
+++ modules/llm/ollama.nix
@@ -10,6 +10,7 @@
    # Ollama Setup
   services.ollama = {
     enable = true;
+    package = pkgs.unstable.ollama;
     acceleration = "cuda";
     loadModels = ["llama3.2:3b" "deepseek-r1:1.5b" "gemma3:12b" "qwen3:8b" "qwen3:14b" "qwen3-coder:latest"];
     openFirewall = true;
