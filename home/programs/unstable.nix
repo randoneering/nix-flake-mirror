33f1727cf3f970069e505cdf76a46720c11e408c
justin <justin@randoneering.tech>
2026-02-17 20:03:40 -0700

    added packer

--- home/programs/unstable.nix
+++ home/programs/unstable.nix
@@ -5,17 +5,9 @@
     # utils
     dbeaver-bin
     obsidian
-    claude-code
-<<<<<<< HEAD
-    codex
     opencode
-    crush
     tmux
     packer
-=======
-    opencode
-    tmux
->>>>>>> f7f4126a54e0a16072183a69dabd64d5f0fda12d
 
     # Security
     syft
