ffd4d8a9407c6627113109fd1f8b60defb7c1f6b
justin <justin@randoneering.tech>
2026-03-03 18:50:45 -0700

    adding postgres just incase :)

--- hosts/lemur/default.nix
+++ hosts/lemur/default.nix
@@ -5,15 +5,18 @@
   lib,
   username,
   ...
-}: {
+}:
+{
   imports = [
     ../../modules/system.nix
+    ../../modules/database/postgresql18.nix
     ../../modules/desktop/gnome/gnome.nix
     ../../modules/networking
     ./hardware-configuration.nix
   ];
 
-  boot.initrd.luks.devices."luks-1f6de9c7-37ee-491d-8639-33efc75ee9d0".device = "/dev/disk/by-uuid/1f6de9c7-37ee-491d-8639-33efc75ee9d0";
+  boot.initrd.luks.devices."luks-1f6de9c7-37ee-491d-8639-33efc75ee9d0".device =
+    "/dev/disk/by-uuid/1f6de9c7-37ee-491d-8639-33efc75ee9d0";
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
@@ -21,27 +24,38 @@
 
   # Flox Settings
   nix.settings.trusted-substituters = [
-    "https://cache.flox.dev"];
+    "https://cache.flox.dev"
+  ];
   nix.settings.trusted-public-keys = [
-    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
-
+    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+  ];
 
   # Enable networking
   networking.networkmanager.enable = true;
   networking.hostName = "nix-lemur";
 
   # Enable NFS
-  boot.supportedFilesystems = ["nfs"];
+  boot.supportedFilesystems = [ "nfs" ];
   services.rpcbind.enable = true; # needed for NFS
   fileSystems."/mnt/jellyfin" = {
     device = "nas.randoneering.cloud:/mnt/randoneering_prod/Jellyfin";
     fsType = "nfs";
-    options = ["x-systemd.automount" "noauto" "x-systemd.after=network-online.target" "x-systemd.mount-timeout=5s"];
+    options = [
+      "x-systemd.automount"
+      "noauto"
+      "x-systemd.after=network-online.target"
+      "x-systemd.mount-timeout=5s"
+    ];
   };
   fileSystems."/mnt/nextcloud" = {
     device = "nas.randoneering.cloud:/mnt/randoneering_prod/NextCloud";
     fsType = "nfs";
-    options = ["x-systemd.automount" "noauto" "x-systemd.after=network-online.target" "x-systemd.mount-timeout=5s"];
+    options = [
+      "x-systemd.automount"
+      "noauto"
+      "x-systemd.after=network-online.target"
+      "x-systemd.mount-timeout=5s"
+    ];
   };
 
   # Enable Bluetooth
---
+++ modules/database/postgres18.nix
@@ -0,0 +1,102 @@
+{
+  config,
+  pkgs,
+  username,
+  hostname,
+  instance,
+  ...
+}:
+
+{
+  systemd.tmpfiles.rules = [
+    "d /var/lib/postgresql/logs 0750 postgres postgres -"
+  ];
+  services.postgresql = {
+    enable = true;
+    package = pkgs.unstable.postgresql_18;
+    extraPlugins = ps: [ ps.pgtap ];
+    ensureDatabases = [ "pgFirstAid" ];
+    enableTCPIP = true;
+    settings = {
+      listen_addresses = "*";
+      cluster_name = "${hostname}/${instance}";
+      ssl = true;
+      ssl_ca_file = "/mnt/postgres/root.crt";
+      ssl_cert_file = "/mnt/postgres/server.crt";
+      ssl_key_file = "/mnt/postgres/server.key";
+      password_encryption = "scram-sha-256";
+
+      ## Logging ##
+      log_destination = "stderr";
+      logging_collector = "on";
+      log_filename = "postgresql-%Y-%m-%d_%H%M%S.log";
+      log_rotation_age = "7d";
+      log_rotation_size = "100MB";
+      log_directory = "/var/lib/postgresql/logs";
+      log_hostname = "on";
+      log_line_prefix = "%m [%p] %q%u@%d";
+      log_lock_waits = "on";
+      log_recovery_conflict_waits = "on";
+      log_statement = "ddl";
+      log_timezone = "Etc/UTC";
+
+      ### When To Log ####
+      log_min_messages = "warning";
+      log_min_error_statement = "error";
+      log_connections = "on";
+      log_disconnections = "on";
+
+      ## Resource Usage ##
+      shared_buffers = "128MB";
+      maintenance_work_mem = "64MB";
+      autovacuum_work_mem = -1;
+      dynamic_shared_memory_type = "posix";
+      min_dynamic_shared_memory = "0MB";
+      vacuum_buffer_usage_limit = "256kB";
+
+      ## WAL Settings ##
+      max_wal_size = "1GB";
+      min_wal_size = "80MB";
+
+      ## Stats ##
+      track_activities = "on";
+      track_activity_query_size = "1024";
+      track_counts = "on";
+      track_io_timing = "on";
+      track_functions = "pl";
+      stats_fetch_consistency = "cache";
+      compute_query_id = "auto";
+      #log_statement_stats = "off";
+      #log_parser_stats = "off";
+      #log_planner_stats = "off";
+      #log_executor_stats = "off";
+
+      ## Autovacuum ##
+      autovacuum = "on";
+      autovacuum_max_workers = "2";
+      autovacuum_vacuum_threshold = "1000";
+      autovacuum_vacuum_insert_threshold = "1000";
+      autovacuum_analyze_threshold = "1000";
+      autovacuum_vacuum_scale_factor = "0.2";
+      autovacuum_vacuum_insert_scale_factor = "0.2";
+      autovacuum_analyze_scale_factor = "0.1";
+
+      ## Client Connection Defaults ##
+      client_min_messages = "notice";
+      datestyle = "iso, mdy";
+      timezone = "Etc/UTC";
+      default_text_search_config = "pg_catalog.english";
+    };
+    port = 5432;
+    authentication = pkgs.lib.mkOverride 10 ''
+      # type database DBuser origin-address auth-method
+      local    all     all     trust
+      # ipv4
+      host     all     all    10.10.1.0/24 scram-sha-256
+      hostssl  all     all    10.10.1.0/24 scram-sha-256
+      # ipv6
+      host     all     all    ::1/128      scram-sha-256
+    '';
+  };
+
+}
