1f7d59586d48a3ec299ba5aa424382433d5d0afd
justin <justin@randoneering.tech>
2026-03-13 14:35:42 -0600

    flox setup

--- hosts/L16/default.nix
+++ hosts/L16/default.nix
@@ -4,7 +4,8 @@
   lib,
   username,
   ...
-}: {
+}:
+{
   imports = [
     ../../modules/system.nix
     ../../modules/desktop/gnome/gnome.nix
@@ -16,10 +17,19 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   # Boot
-  boot.initrd.luks.devices."luks-f9c32740-0c68-4913-9a0f-d26df56b3aa1".device = "/dev/disk/by-uuid/f9c32740-0c68-4913-9a0f-d26df56b3aa1";
+  boot.initrd.luks.devices."luks-f9c32740-0c68-4913-9a0f-d26df56b3aa1".device =
+    "/dev/disk/by-uuid/f9c32740-0c68-4913-9a0f-d26df56b3aa1";
   networking.hostName = "nix-l16";
   networking.networkmanager.enable = true;
 
+  # Flox Settings
+  nix.settings.trusted-substituters = [
+    "https://cache.flox.dev"
+  ];
+  nix.settings.trusted-public-keys = [
+    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    "flox-signing:s1KtIMsNrdGeYeLPdzQXDTyXmMbBII1rPVvZgll6dqE="
+  ];
   # Steam
   programs.steam = {
     enable = true;
@@ -30,19 +40,32 @@
   # Enable 32bit
   hardware.graphics.enable32Bit = true;
   # Enable NFS
-  boot.supportedFilesystems = ["nfs"];
+  boot.supportedFilesystems = [ "nfs" ];
   services.rpcbind.enable = true; # needed for NFS
   fileSystems."/mnt/jellyfin" = {
     device = "nas.randoneering.cloud:/mnt/randoneering_prod/Jellyfin";
     fsType = "nfs";
-    options = ["x-systemd.automount" "noauto" "x-systemd.after=network-online.target" "x-systemd.mount-timeout=5s"];
+    options = [
+      "x-systemd.automount"
+      "noauto"
+      "x-systemd.after=network-online.target"
+      "x-systemd.mount-timeout=5s"
+    ];
   };
   fileSystems."/mnt/nextcloud" = {
     device = "nas.randoneering.cloud:/mnt/randoneering_prod/NextCloud";
     fsType = "nfs";
-    options = ["x-systemd.automount" "noauto" "x-systemd.after=network-online.target" "x-systemd.mount-timeout=5s"];
+    options = [
+      "x-systemd.automount"
+      "noauto"
+      "x-systemd.after=network-online.target"
+      "x-systemd.mount-timeout=5s"
+    ];
   };
-  nix.settings.experimental-features = ["nix-command" "flakes"];
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
 
   # Enable Bluetooth
   hardware.bluetooth.enable = true;
