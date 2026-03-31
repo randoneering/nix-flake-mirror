307d9591d4f4588577b5db9a0b274991a28e5c64
justin <justin@randoneering.tech>
2026-03-31 13:20:59 -0600

    home manager now follows unstable

--- home/programs/opencode.nix
+++ home/programs/opencode.nix
@@ -10,9 +10,9 @@
 
   programs.opencode = {
     enable = true;
-    skills = "${opencode-config}/skills";
+    skills = "${opencode-config}/skills/*";
     rules = "./opencode/AGENTS.md";
-    agents = "${opencode-config}/agents";
+    agents = "${opencode-config}/agents/*";
     package = pkgs.unstable.opencode;
     settings = {
       autoupdate = true;
