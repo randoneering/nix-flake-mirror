6442906aba83c11be90c66d30a23dd81bab37a85
justin <justin@randoneering.tech>
2026-04-14 22:02:31 -0600

    adding ollama to pi?

--- home/programs/pi-coding-agent.nix
+++ home/programs/pi-coding-agent.nix
@@ -11,52 +11,59 @@ in {
     pkgs.unstable."pi-coding-agent"
   ];
 
-  home.file = {
-    ".pi/agent/AGENTS.md".source = "${agent-config}/AGENTS.md";
-    ".pi/agent/skills" = {
-      source = "${agent-config}/skills";
-      recursive = true;
-    };
-    ".pi/agent/models.json".text = builtins.toJSON {
-      providers = {
-        ollama = {
-          baseUrl = "https://ollama.randoneering.dev/v1";
-          apiType = "openai-completions";
-          apiKey = "ollama";
-          models = [
-            {id = "qwen3.5:4b";}
-            {id = "gemma4:e2b";}
-          ];
+  home.file =
+    {
+      ".pi/agent/AGENTS.md".source = "${agent-config}/AGENTS.md";
+      ".pi/agent/skills" = {
+        source = "${agent-config}/skills";
+        recursive = true;
+      };
+      ".pi/agent/models.json".text = builtins.toJSON {
+        providers = {
+          ollama = {
+            baseUrl = "https://ollama.randoneering.dev/v1";
+            apiType = "openai-completions";
+            apiKey = "ollama";
+            models = [
+              {"id" = "qwen3.5:4b";}
+              {"id" = "gemma4:e2b";}
+            ];
+          };
         };
       };
-    };
-    ".pi/agent/settings.json".text = builtins.toJSON {
-      defaultProvider = "ollama";
-      model = "qwen3.5:4b";
-    };
-  }
-    // (if builtins.pathExists promptsDir
-    then {
-      ".pi/agent/prompts" = {
-        source = promptsDir;
-        recursive = true;
+      ".pi/agent/settings.json".text = builtins.toJSON {
+        defaultProvider = "ollama";
+        model = "qwen3.5:4b";
       };
     }
-    else {})
-    // (if builtins.pathExists extensionsDir
-    then {
-      ".pi/agent/extensions" = {
-        source = extensionsDir;
-        recursive = true;
-      };
-    }
-    else {})
-    // (if builtins.pathExists themesDir
-    then {
-      ".pi/agent/themes" = {
-        source = themesDir;
-        recursive = true;
-      };
-    }
-    else {});
+    // (
+      if builtins.pathExists promptsDir
+      then {
+        ".pi/agent/prompts" = {
+          source = promptsDir;
+          recursive = true;
+        };
+      }
+      else {}
+    )
+    // (
+      if builtins.pathExists extensionsDir
+      then {
+        ".pi/agent/extensions" = {
+          source = extensionsDir;
+          recursive = true;
+        };
+      }
+      else {}
+    )
+    // (
+      if builtins.pathExists themesDir
+      then {
+        ".pi/agent/themes" = {
+          source = themesDir;
+          recursive = true;
+        };
+      }
+      else {}
+    );
 }
