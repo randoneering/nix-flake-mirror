1575f4fada053983c50a8f8f2f19e50e028199b2
justin <justin@randoneering.tech>
2026-05-22 20:05:39 -0600

    fix(app): fix sidra

--- home/programs/common.nix
+++ home/programs/common.nix
@@ -1,62 +1,83 @@
-{pkgs, ...}: let
+{
+  pkgs,
+  sidra,
+  ...
+}: let
   floxMcpWrapper = pkgs.writeShellScriptBin "flox-mcp" ''
     unset PYTHONPATH PYTHONHOME PYTHONNOUSERSITE VIRTUAL_ENV
     exec flox activate -r flox/flox-mcp-server -- flox-mcp "$@"
   '';
+
+  sidraPackage =
+    if pkgs.stdenv.hostPlatform.system == "x86_64-linux" then
+      pkgs.callPackage "${sidra}/nix/linux.nix" {
+        version = (pkgs.lib.importJSON "${sidra}/package.json").version;
+        fetchurl = args:
+          pkgs.fetchurl (
+            args
+            // {
+              hash = "sha256-QqCH6ln75uTc910w8MorgSBcNuZjyfjgyCmgOtiJk3M=";
+            }
+          );
+      }
+    else
+      sidra.packages.${pkgs.stdenv.hostPlatform.system}.default;
 in {
-  home.packages = with pkgs; [
-    # archives
-    zip
-    gtk4
-    gtk3
-    sassc
-    meson
-    libglibutil
-    dig
-    # utils
-    btop
-    dpkg
-    ripgrep
-    nfs-utils
-    git
-    wget
-    awscli2
-    google-cloud-sdk
-    gh
-    popsicle
-    dconf
-    freshfetch
-    gimp3
-    nmap
-    azure-cli
-    # python
-    ruff
-    uv
+  home.packages =
+    [sidraPackage]
+    ++ (with pkgs; [
+      # archives
+      zip
+      gtk4
+      gtk3
+      sassc
+      meson
+      libglibutil
+      dig
+      # utils
+      btop
+      dpkg
+      ripgrep
+      nfs-utils
+      git
+      wget
+      awscli2
+      google-cloud-sdk
+      gh
+      popsicle
+      dconf
+      freshfetch
+      gimp3
+      nmap
+      azure-cli
+      # python
+      ruff
+      uv
 
-    # nix dev
-    niv
-    nixpkgs-review
+      # nix dev
+      niv
+      nixpkgs-review
 
-    # IDE/Database Manager
-    mongosh
-    postgresql
-    mysql80
-    pgformatter
+      # IDE/Database Manager
+      mongosh
+      postgresql
+      mysql80
+      pgformatter
 
-    # IaC
-    opentofu
+      # IaC
+      opentofu
 
-    # Configuration Management
-    ansible
+      # Configuration Management
+      ansible
 
-    # Steam?
-    glibc
+      # Steam?
+      glibc
 
-    # Security
-    proton-pass
-    proton-authenticator
-    protonvpn-gui
-    protonmail-desktop
-    floxMcpWrapper
-  ];
+      # Security
+      proton-pass
+      proton-authenticator
+      protonvpn-gui
+      protonmail-desktop
+      floxMcpWrapper
+    ]);
 }
--- home/programs/unstable.nix
+++ home/programs/unstable.nix
@@ -1,54 +1,48 @@
-{
-  pkgs,
-  sidra,
-  ...
-}: {
-  home.packages =
-    [sidra.packages.${pkgs.stdenv.hostPlatform.system}.default]
-    ++ (with pkgs.unstable; [
-      # utils
-      dbeaver-bin
-      obsidian
-      tmux
-      packer
-      openstackclient
-      qemu
-      jq
-      nodejs
-      zed-editor
-      lmstudio
-      atuin-desktop
-      # Go
-      go
-      # JavaScript
-      pnpm
-      biome
-      # k8s/Containers
-      kubectl
-      docker
-      docker-compose
-      talosctl
-      omnictl
-      helm
-      kustomize
-      k9s
-      # Security
-      prowler
+{pkgs, ...}: {
+  home.packages = with pkgs; [
+    # utils
+    dbeaver-bin
+    obsidian
+    tmux
+    packer
+    openstackclient
+    qemu
+    jq
+    nodejs
+    zed-editor
+    lmstudio
+    atuin-desktop
+    # Go
+    go
+    # JavaScript
+    pnpm
+    biome
+    # k8s/Containers
+    kubectl
+    docker
+    docker-compose
+    talosctl
+    omnictl
+    helm
+    kustomize
+    k9s
+    # Security
+    prowler
 
-      # Static Site
-      hugo
+    # Static Site
+    hugo
 
-      # Desktop Customization
-      gnome-tweaks
-      gnome-extension-manager
+    # Desktop Customization
+    gnome-tweaks
+    gnome-extension-manager
 
-      # Social
-      discord
-      slack
+    # Social
+    discord
+    slack
 
-      # database-stuff
-      duckdb
-      # Office
-      onlyoffice-desktopeditors
-    ]);
+    # database-stuff
+    duckdb
+    # Office
+    onlyoffice-desktopeditors
+  ];
 }
